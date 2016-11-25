86aaa9e72074e4fb44d07a75f4c863e076b19f31
JensNordenbro <jens.nordenbro@gmail.com>
2016-11-25 23:35:11 +0100

    add FW4.6 in order to test compatibility

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ActoR;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Task.Run(async () =>
+            {
+                var actor = ActorFactory.Create(ActorAffinity.LongRunningThread);
+                await actor.Do(() => Console.WriteLine($"hello from thread {Thread.CurrentThread.ManagedThreadId}"));
+            }).Wait();
+            Console.ReadLine();
+            
+        }
+    }
+}
